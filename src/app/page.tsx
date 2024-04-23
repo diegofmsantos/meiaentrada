a3c542f99bd1dd93a63d36ab8f4ff0405ba606ef
Diego Santos <diegofmsantos0@gmail.com>
2024-04-23 15:51:23 -0300

    ajustes responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const Page = () => {
     <div className="overflow-x-hidden">
       <Header />
       <div className="p-3 flex flex-col mt-20 md:px-7">
-        <section className="flex flex-col justify-start gap-1 mb-2 ml-36 xl:flex-row xl:items-center xl: my-8 xl:gap-2">
+        <section className="flex flex-col justify-start gap-1 mb-2 xl:flex-row xl:items-center xl:my-8 xl:ml-36 xl:mb-2 xl:gap-2">
           <div className="text-start mt-8 text-[28px] text-[#005E9E] font-semibold xl:mt-0">
             Validação de CIE
           </div>
@@ -20,28 +20,28 @@ const Page = () => {
         <div className="max-w-[1100px] w-full mt-2 md:flex md:gap-4 xl:m-auto xl:justify-start xl:items-start">
           <div className="flex-1">
             <div className="flex flex-col items-start justify-center border-y mb-2 h-15 md:flex-row md:gap-4 md:justify-start md:h-10 md:items-center md:text-lg">
-              <span className="font-semibold xl:ml-36">Nome:</span>
+              <span className="font-semibold lg:ml-36">Nome:</span>
               <div className="xl:ml-12 xl:font-thin"> Haim Ferreira Araújo Braz</div>
             </div>
             <div className="flex flex-col items-start justify-center border-b mb-2 h-15 md:flex-row md:gap-4 md:justify-start md:h-10 md:items-center md:text-lg">
-              <span className="font-semibold xl:ml-36">Instituição:</span>
+              <span className="font-semibold lg:ml-36">Instituição:</span>
               <div className="text-sm lg:text-lg xl:ml-12 xl:font-thin"> Centro Universitario Mauricio de Nassau</div>
             </div>
             <div className="flex flex-col items-start justify-center border-b mb-2 h-15 md:flex-row md:gap-4 md:justify-start md:h-10 md:items-center md:text-lg">
-              <span className="font-semibold xl:ml-36">Curso:</span>
-              <div className=" xl:ml-12 xl:font-thin"> Analise e Desenvolvimento de S</div>
+              <span className="font-semibold lg:ml-36">Curso:</span>
+              <div className="xl:ml-12 xl:font-thin"> Analise e Desenvolvimento de S</div>
             </div>
             <div className="flex flex-col items-start justify-center border-b mb-2 h-15 md:flex-row md:gap-4 md:justify-start md:h-24 xl:h-16 md:text-lg">
-              <span className="font-semibold md:w-20 md:text-center xl:w-40 xl:ml-24">Documento de Identificação:</span>
+              <span className="font-semibold md:w-20 md:text-center xl:w-40 lg:ml-36 xl:ml-24">Documento de Identificação:</span>
               <div className="md:ml-8 xl:ml-12 xl:font-thin"> 000000008118287</div>
             </div>
             <div className="flex flex-col items-start justify-center border-b mb-2 h-15 md:flex-row md:gap-4 md:justify-start md:h-10 md:items-center md:text-lg">
-              <span className="font-semibold xl:ml-36">Emissor:</span>
+              <span className="font-semibold lg:ml-36">Emissor:</span>
               <div className="xl:ml-12 xl:font-thin"> UNE</div>
             </div>
           </div>
-          <div className="w-35 h-35 m-auto flex justify-center items-center max-w-[375]:w-40 md:w-52 lg:w-36 xl:w-[160px] xl:h-[213px]">
-            <Image className="w-35 h-35 rounded-md max-w-[375]:w-40 md:w-52 lg:w-36 xl:w-[160px] xl:h-[213px]" src="/assets/haim.jpeg" width={100} height={150} alt="Haim" />
+          <div className="w-30 h-30 m-auto flex justify-center items-center max-w-[375]:w-40 md:w-52 lg:w-36 xl:w-[160px] xl:h-[213px]">
+            <Image className="w-30 h-30 rounded-md max-w-[375]:w-40 md:w-52 lg:w-36 xl:w-[160px] xl:h-[213px]" src="/assets/haim.jpeg" width={100} height={150} alt="Haim" />
           </div>
         </div>
         <section className="max-w-[1100px] m-auto mt-4">
